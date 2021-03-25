801ce83ad088484125623dcae29acdcbbfe71bae
si3mshady <cloudwarrior2891@gmail.com>
2021-03-24 20:08:12 -0500

    adding eks cluster wip

--- src/Axios.js
+++ src/Axios.js
@@ -2,6 +2,8 @@ import axios from "axios"
 
 const instance = axios.create({
     baseURL: "http://localhost:9000"
+    // baseURL: "http://node:9000",
+    // baseURL: "http://3.15.172.225:9000" 
 })
 
 export default instance 
