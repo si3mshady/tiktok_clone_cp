7feb8f3b6b82f5e1099cfa20941a0d86cc152955
si3mshady <cloudwarrior2891@gmail.com>
2021-03-25 07:07:39 -0500

    added service type loadbalancer for node backend

--- src/Axios.js
+++ src/Axios.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "http://localhost:9000"
+    // baseURL: "http://localhost:9000" 
     // baseURL: "http://node:9000",
-    // baseURL: "http://3.15.172.225:9000" 
+    baseURL: "http://a55e21a2183424346bd34da9921ff7ac-285514034.us-east-2.elb.amazonaws.com:9000" 
 })
 
 export default instance 
