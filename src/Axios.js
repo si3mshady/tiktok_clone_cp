f01196d8af4b2a56cf51096eab0057e5df5059a8
si3mshady <cloudwarrior2891@gmail.com>
2021-03-26 14:52:23 -0500

    final commit

--- src/Axios.js
+++ src/Axios.js
@@ -2,8 +2,9 @@ import axios from "axios"
 
 const instance = axios.create({
     // baseURL: "http://localhost:9000" 
-    // baseURL: "http://node:9000",
-    baseURL: "http://a55e21a2183424346bd34da9921ff7ac-285514034.us-east-2.elb.amazonaws.com:9000" 
+    // baseURL: "http://node:9000"
+    baseURL: "http://a84424ebf3e764cdd8139c80f47a7913-703231666.us-east-2.elb.amazonaws.com:9000" 
+    
 })
 
 export default instance 
